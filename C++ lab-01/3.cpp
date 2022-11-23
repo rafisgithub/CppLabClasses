92fc49ce6a9394a21221def6edff7f3a0ec9b66f
rafi-ahmed <smrafi120@gmail.com>
2022-11-23 22:20:10 +0600

    first commit

--- C++ lab-01/3.cpp
+++ C++ lab-01/3.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int num1,num2;
-cout<<"enter two number:";
-cin>>num1>>num2;
-
-cout<<"Sum ="<<num1+num2;
+	int num1,num2,sum=0;
+    cout<<"enter two number:";
+    cin>>num1>>num2;
+    sum=num1+num2;
+    cout<<"Sum ="<<num1+num2;
+    return 0;
 }
